d128dce51fc3b22d4fff4402b8d7f90108b595d3
lubgade <lubgade@yahoo.com>
2017-03-25 16:29:23 -0400

    After review

--- handlers/permalink.py
+++ handlers/permalink.py
@@ -3,7 +3,6 @@ from main import *
 from main import CookieFunctions
 
 
-
 class PermalinkHandler(Handler, CookieFunctions):
     def get(self, id):
         cookie_val = self.cookie_check()
@@ -12,11 +11,8 @@ class PermalinkHandler(Handler, CookieFunctions):
         else:
             user = ""
         e = Blog.get_blog_entry(id)
-        comments_list = []
         if e:
-            for k in e.comments:
-                if e.comments:
-                    comments_list.append(Comment.get_comment(k))
+            comments_list = Comment.get_comments_list(e, 0)
             self.render('permalink.html', e=e, id=id, cookie_val=cookie_val,
                         user=user, comments_list=comments_list)
         else:
