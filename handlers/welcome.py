af54f243e69daba563764fec4f5739305741abef
lubgade <lubgade@yahoo.com>
2017-03-29 18:43:44 -0400

    Implemented one-to-many for author of the post

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -7,8 +7,8 @@ class WelcomePageHandler(Handler, CookieFunctions):
     def get(self):
         cookie_val = self.cookie_check()
         if cookie_val:
-            user = User.get_user_name(cookie_val)
-            self.render("welcometemplate.html", name=user, user=user,
+            user = User.get_user(cookie_val)
+            self.render("welcometemplate.html", name=user.name, user=user.name,
                         cookie_val=cookie_val)
         else:
             self.redirect('/blog/signup')
