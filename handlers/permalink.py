7e4180c5c8329c5f1c0a7f81f0b9c945897ae4a4
lubgade <lubgade@yahoo.com>
2017-03-29 18:44:17 -0400

    Implemented one-to-many for author of the post

--- handlers/permalink.py
+++ handlers/permalink.py
@@ -7,13 +7,13 @@ class PermalinkHandler(Handler, CookieFunctions):
     def get(self, id):
         cookie_val = self.cookie_check()
         if cookie_val:
-            user = User.get_user_name(cookie_val)
+            user = User.get_user(cookie_val)
         else:
             user = ""
         e = Blog.get_blog_entry(id)
         if e:
             comments_list = Comment.get_comments_list(e, 0)
             self.render('permalink.html', e=e, id=id, cookie_val=cookie_val,
-                        user=user, comments_list=comments_list)
+                        user=user.name, comments_list=comments_list)
         else:
             self.redirect('/blog')
