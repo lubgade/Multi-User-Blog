c8cc67020a19b73352a487eb175f74f737102603
lubgade <lubgade@yahoo.com>
2017-03-29 18:45:09 -0400

    Implemented one-to-many for author of the post

--- handlers/newpost.py
+++ handlers/newpost.py
@@ -2,38 +2,41 @@ from main import *
 from handler import Handler
 from main import CookieFunctions
 import bleach
+# noinspection PyUnresolvedReferences
+from google.appengine.ext import db
 
 
 class NewpostHandler(Handler, CookieFunctions):
     def get(self):
         cookie_val = self.cookie_check()
         if cookie_val:
-            user = User.get_user_name(cookie_val)
-            self.render("newpost.html", cookie_val=cookie_val, user=user)
+            user = User.get_user(cookie_val)
+            self.render("newpost.html", cookie_val=cookie_val, user=user.name)
             return
         self.redirect('/blog/signup')
 
     def post(self):
-        val = ""
         cookie_val = self.cookie_check()
         if cookie_val:
-            val = User.get_user_name(cookie_val)
-        else:
-            cookie_val = ""
+            user = User.get_user(cookie_val)
 
-        subject = self.request.get("subject")
-        subject = bleach.clean(subject, tags=tags_title)
-        content = self.request.get("content")
-        content = content.replace('\n', '<br>')
-        content = bleach.clean(content, tags=tags_content)
+            subject = self.request.get("subject")
+            subject = bleach.clean(subject, tags=tags_title)
+            content = self.request.get("content")
+            content = content.replace('\n', '<br>')
+            content = bleach.clean(content, tags=tags_content)
+            author = user
 
-        if subject and content:
-            entry = Blog(subject=subject, content=content, author=val,
-                         liked_by=[], disliked_by=[])
-            entry.put()
-            id = entry.key().id()
-            self.redirect('/blog/%s' % str(id))
+            if subject and content:
+                entry = Blog(subject=subject, content=content,author=author,
+                             liked_by=[], disliked_by=[])
+                entry.put()
+                id = entry.key().id()
+                self.redirect('/blog/%s' % str(id))
+            else:
+                error = "subject and content, please"
+                self.render("newpost.html", error=error, subject=subject,
+                            content=content, cookie_val=cookie_val,
+                            user=user.name)
         else:
-            error = "subject and content, please"
-            self.render("newpost.html", error=error, subject=subject,
-                        content=content, cookie_val=cookie_val, user=val)
+            self.redirect('/blog/login')
